7d002359c0c75c6e3f58398263bb0708f55e1cfd
adelarja <adelarja@gmail.com>
2022-04-26 00:03:07 -0300

    Added events for allowance changes

--- projects/wallet/wallet.sol
+++ projects/wallet/wallet.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.13;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
 contract Allowance is Ownable {
+    event AllowanceChanged(address indexed _forWho, address indexed _fromWhom, uint _oldAmount, uint _newAmount);
     mapping(address => uint) public allowance;
 
     modifier ownerOrAllowed(uint _amount) {
@@ -12,6 +13,7 @@ contract Allowance is Ownable {
     }
 
     function reduceAllowance(address _who, uint _amount) internal {
+        emit AllowanceChanged(_who, msg.sender, allowance[_who], allowance[_who] - _amount);
         allowance[_who] -= _amount;
     }
 
@@ -20,6 +22,7 @@ contract Allowance is Ownable {
     }
 
     function addAllowance(address _who, uint _amount) public onlyOwner {
+        emit AllowanceChanged(_who, msg.sender, allowance[_who], _amount);
         allowance[_who] = _amount;
     }
 }
